f2b545ff824f38005c06d31498b1aadd821fdb80
KHR-gitit <khalilrohanidev@gmail.com>
2021-11-25 14:44:05 +1100

    first day of the 100 day python coding challenge

---
+++ first_day.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("welcome to your band name generator")
+#2. Ask the user for the city that they grew up in.
+name_of_city = input("what is the name of the city you grow up in? \n")
+#3. Ask the user for the name of a pet.
+name_of_pet = input("what is your pet's name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + name_of_city + " " + name_of_pet)
