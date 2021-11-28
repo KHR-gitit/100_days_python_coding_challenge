401ef8e8609c8df21b5d1fc4efe9d061807f4414
KHR-gitit <khalilrohanidev@gmail.com>
2021-11-29 01:03:26 +1100

    the thrid of 100 days of python coding challeng

---
+++ fourth_day/coin_flip.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+
+num = random.randint(1,2)
+
+if num == 1:
+  print("head")
+else:
+  print("tail")
